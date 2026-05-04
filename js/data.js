a77f336d9136b0e7a984526c53efdcf7ab41fe59
maurodure <meoflux2.0@gmail.com>
2026-05-04 18:39:25 +0200

    Fix favicon e icono app

--- js/data.js
+++ js/data.js
@@ -38,14 +38,14 @@ const DATA = {
       maps: "https://maps.google.com/?q=Mercado+de+Pulgas+del+Bajo+Rosario+Belgrano+y+Buenos+Aires"
     },
     {
-      nombre: "Feria del Bajo",
-      tipo: "Arte urbano, música en vivo",
-      dias: { es: "Sábados y domingos", en: "Saturdays and Sundays", pt: "Sábados e domingos" },
-      horario: "16 a 21hs",
+      nombre: "calle Recreativa",
+      tipo: "Paseo al aire libre, Arte urbano, música en vivo",
+      dias: { es: "Domingos", en: "Sundays", pt: "Domingos" },
+      horario: "9 a 12hs",
       icono: "🎵",
-      lat: -32.9431,
-      lng: -60.6288,
-      maps: "https://maps.google.com/?q=-32.9431,-60.6288"
+      lat: -32.9309063,
+      lng: -60.6500612,
+      maps: "https://maps.google.com/?q=-32.9309063,-60.6500612"
     },
     {
       nombre: "Feria del Boulevard",
@@ -63,9 +63,9 @@ const DATA = {
       dias: { es: "Lunes a sábados", en: "Monday to Saturday", pt: "Segunda a sábado" },
       horario: "8 a 20hs",
       icono: "assets/logos/mercado-patio.png",
-      lat: -32.9372173,
-      lng: -60.6657257,
-      maps: "https://maps.google.com/?q=-32.9372173,-60.6657257"
+      lat: -32.9397493,
+      lng: -60.6701945,
+      maps: "https://maps.google.com/?q=-32.9397493,-60.6701945"
     }
   ],
 
